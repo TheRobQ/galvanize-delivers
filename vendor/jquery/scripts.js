918d1a0f36ed3dafb5818ad47abcbbd1552b3326
Robert Quan <rob.quan@gmail.com>
2017-10-22 15:18:02 -0600

    All basic functionality in place, need CSS anbd design clean up

--- vendor/jquery/scripts.js
+++ vendor/jquery/scripts.js
@@ -1,47 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function() {
   var total = [];
   var order = [];
 
+  $('button').click(function(event) {
+    var accumulator = 0;
+    var myTotal = 0;
+    var price = $(event.target).closest('.portfolio-item').find('.price').text();
+    price = (price.replace(/\$/g, ''));
+    total.push(parseFloat(price))
+    var addItem = $(event.target).closest('.portfolio-item').find('.card-title').text();
+    order.push(addItem);
+    //console.log(order);
+    //console.log(total);
 
-$('button').click(function(event){
-  var myTotal = 0;
-  var price = $(event.target).closest('.portfolio-item').find('.price').text();
-  price = (price.replace(/\$/g, ''));
-  total.push(parseFloat(price))
-  var addItem = $(event.target).closest('.portfolio-item').find('.card-title').text();
-  order.push(addItem);
-  console.log(order);
-  console.log(total);
+    $('#yourOrder').after('<p class="orderItem">' + addItem + ': $' + price + '</p>');
 
-  $('#yourOrder').after('<p class="orderItem">' + addItem + ': $' + price + '</p>');
+    total.forEach(function(price) {
+      accumulator += price
+    })
+    var yourSubTotal = accumulator.toFixed(2);
+    //console.log(yourTotal);
+    $('#yourSubTotal').replaceWith('<h5 id="yourSubTotal">Subtotal: $' + yourSubTotal + '</h5>');
+    var tax = (yourSubTotal * .087).toFixed(2);
+    $('#yourTax').replaceWith('<h5 id="yourTax">Tax: $' + tax + '</h5>');
+    var final = (parseFloat(yourSubTotal) + parseFloat(tax));
+    $('#yourTotal').replaceWith('<h3 id="yourTotal">Total: $' + final.toFixed(2) + '</h3>');
+
+  })
 })
-
-$('#total').click(function(event){
-  event.preventDefault();
-  accumulator = 0;
-  for(let i = 0; i < total.length; i++){
-    accumulator += total[i];
-  }
-  var yourTotal = accumulator.toFixed(2)
-  console.log(yourTotal);
-  return yourTotal;
-})
-
-
-
-
-})
-// for (var i = 0; i < colors.length; i++) {
-//   var div = document.createElement('div');
-//   div.style.float = 'left';
-//   div.style.paddingBottom = '6%';
-//   div.style.width = '6%';
-//   div.style.borderColor = 'LightGray';
-//   //div.style.borderRadius = '50px;'
-//   div.style.borderStyle = 'solid';
-//   div.style.borderWidth = '.5px';
-//   div.style.marginLeft = '4px';
-//   div.style.backgroundColor = colors[i];
-//   div.className = "circle"
-//   paints.appendChild(div)
-// }
